5c0193b762441f363d06f0bd27f61b0121b22043
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-11 19:52:10 -0500

    Project

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,13 +2,73 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movies_from_api(character_name) # => an array of hashes of movies
+  my_film_hash = []
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  # names_array = response_hash["results"]
+  response_hash["results"].each do |person|
+    if person["name"].downcase == character_name
+      # my_films = person_hash["films"]
+      person["films"].each do |film|
+        url = RestClient.get(film)
+        parser = JSON.parse url
+        my_film_hash << parser
+      end
+    end
+  end
+  my_film_hash
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #end
+#   names_array.each do |key,value|
+#     puts key == :name ? "#{key} : \n" : "#{value}"
+#
+# my_film_hash = []
+#   response_string = RestClient.get('http://www.swapi.co/api/people')
+#   response_hash = JSON.parse(response_string)
+#   names_array = response_hash["results"]
+#   names_array.each do |person_hash|
+#     if person_hash["name"] == character_name
+#       my_films = person_hash["films"]
+#
+#       my_films.each do |film|
+#         response = RestClient.get film
+#         json_film = JSON.parse response
+#         film_hashes << json_film
+#
+#     end
+#   end
+# end
+#   my_film_hash
+#   binding.pry
+# end
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
+# name = response_hash["results"][0]["name"]
+# films = response_hash["results"][0] ["films"]
+
+
+
+
+
+
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -16,10 +76,16 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
 
 def print_movies(films)
+  new_empty = []
+  films.each do|film|
+    new_empty << film["title"]
   # some iteration magic and puts out the movies in a nice list
+  end
+  new_empty
+  binding.pry
 end
 
 def show_character_movies(character)
